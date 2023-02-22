8357021538d76af28af9fc0e6f454f7e924f04fd
Fahed <114879775+ALJURDY@users.noreply.github.com>
2023-02-22 17:07:58 +0100

    Header CSS Valider l'idéntification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { CartComponent } from './pages/cart/cart.component';
 import { CommandComponent } from './pages/command/command.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IdentificationComponent } from './identification/identification.component';
+
 import { ProductsCategoryComponent } from './components/products/products-category/products-category.component';
 import { TagsComponent } from './components/products/tags/tags.component';
 import { ProductCardComponent } from './components/products/product-card/product-card.component';
@@ -31,16 +34,17 @@ import { BaseProductComponent } from './components/custom-product/base-product/b
     NavBarComponent,
     ProductsCategoryComponent,
     TagsComponent,
-    ProductCardComponent
+    ProductCardComponent,
     BuonAppetitoComponent,
     CustomizeParentComponent,
     ProductExtraComponent,
     BaseProductComponent,
-   
+    IdentificationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
